168121ab51e65ffac2475b9d6ae51b2d58618c51
NIKHIL VERMA <99395398+Nikk4507@users.noreply.github.com>
2022-04-18 23:28:39 +0530

    Add files via upload

---
+++ shop.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class shop{
+    int itemId[100];
+    int itemPrice[100];
+    int counter,n;
+    public:
+    void initcounter(void);
+    void SetPrice(void);
+    void DisplayPrice(void);
+};
+
+void shop :: initcounter(){
+    counter = 0;
+}
+
+void shop :: SetPrice(){
+    cout<<"Enter the ItemId of "<<counter+1<<" is :";
+    cin>>itemId[counter];
+    cout<<"Enter the ItemPrice of product "<<counter+1<<" is: ";
+    cin>>itemPrice[counter];
+    counter++;
+}
+
+void shop :: DisplayPrice(){
+    for (int i = 0; i < counter; i++)
+    {
+        cout<<"The ItemId is "<<itemId[i]<<" and Itemprice is: "<<itemPrice[i]<<endl;
+    }
+    
+}
+
+int main(){
+    shop dukaan;
+    dukaan.initcounter();
+    dukaan.SetPrice();
+    dukaan.DisplayPrice();
+}
